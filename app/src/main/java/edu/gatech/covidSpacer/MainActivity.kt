1145bafb9d29685185c69550407eac73701e1857
Kurfess <ghkurfess@gmail.com>
2020-05-20 11:47:48 -0400

    Updated UI features, randomized minor and major axis

--- app/src/main/java/edu/gatech/covidSpacer/MainActivity.kt
+++ app/src/main/java/edu/gatech/covidSpacer/MainActivity.kt
@@ -1,12 +1,28 @@
 package edu.gatech.covidSpacer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val majorNum = findViewById<TextView>(R.id.majorNum)
+        val minorNum = findViewById<TextView>(R.id.minorNum)
+
+        val minor: Int = randomGenerate()
+        val major: Int = randomGenerate()
+
+        minorNum.text = minor.toString()
+        majorNum.text = major.toString()
+    }
+    private fun randomGenerate(): Int {
+
+        val bound = 65536
+        //generate random values from 0-65536
+        return Random(System.nanoTime()).nextInt(bound)
     }
 }
