adc24cf9ac8f198f339a390abf6c4aba2e48ee49
danielur3 <danielur3@gmail.com>
2020-05-21 12:49:32 -0500

    Added permissions to manifest Added libraries foe beacon and scan Added the functionality of the previous app to the MainActivity Fixed a problem with the commit
    Next: Connect UI to Functionality of Main Activity

--- app/src/main/java/edu/gatech/covidSpacer/MainActivity.kt
+++ app/src/main/java/edu/gatech/covidSpacer/MainActivity.kt
@@ -1,11 +1,24 @@
 package edu.gatech.covidSpacer
 
+import android.Manifest
+import android.bluetooth.le.ScanResult
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.uepb.nutes.simpleblescanner.SimpleBleScanner
+import br.edu.uepb.nutes.simpleblescanner.SimpleScannerCallback
+import com.uriio.beacons.Beacons
+import com.uriio.beacons.model.iBeacon
+import java.util.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSION_REQUEST_COARSE_LOCATION = 1
+    private val PERMISSION_REQUEST_FINE_LOCATION = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,18 +26,135 @@ class MainActivity : AppCompatActivity() {
         val majorNum = findViewById<TextView>(R.id.majorNum)
         val minorNum = findViewById<TextView>(R.id.minorNum)
 
-        //checking commit
 
         val minor: Int = randomGenerate()
         val major: Int = randomGenerate()
 
         minorNum.text = minor.toString()
         majorNum.text = major.toString()
+
+
+        // Make sure we have access coarse location enabled, if not, prompt the user to enable it
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            val builder =
+                AlertDialog.Builder(this)
+            builder.setTitle("This app needs location access")
+            builder.setMessage("Please grant location access so this app can detect peripherals.")
+            builder.setPositiveButton("ok", null)
+            builder.setOnDismissListener {
+                requestPermissions(
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                    PERMISSION_REQUEST_COARSE_LOCATION
+                )
+            }
+            builder.show()
+        }
+
+        // Make sure we have access coarse location enabled, if not, prompt the user to enable it
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            val builder =
+                AlertDialog.Builder(this)
+            builder.setTitle("This app needs location access")
+            builder.setMessage("Please grant location access so this app can detect peripherals.")
+            builder.setPositiveButton("ok", null)
+            builder.setOnDismissListener {
+                requestPermissions(
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSION_REQUEST_FINE_LOCATION
+                )
+            }
+            builder.show()
+        }
+
+        //Initialize beacon and assign a UUID, in my original application the beacon starts when the app starts,
+        // now we want it to start when button?
+
+
+        Beacons.initialize(this)
+
+        fun byteArrayOfInts(vararg ints: Int) = ByteArray(ints.size) { pos -> ints[pos].toByte() }
+        val arr = byteArrayOfInts(0xA1, 0x2E, 0x38, 0xD4, 0x89, 0xC3) //EXAMPLE
+        iBeacon(arr, 1000, 100).start() //EXAMPLE
+
+        Log.d("MainActivity", "About to initialize scan")
+
+        // Lets set up the scan
+
+        val mScanner = SimpleBleScanner.Builder()
+            .addScanPeriod(15000) // 15s in milliseconds
+            .build()
+
+        Log.d("MainActivity", "About to start scan")
+
+        // Originally the scan starts when push button , I understand that now we wanting to start with the app?
+
+        //button.setOnClickListener {
+            mScanner.startScan(object : SimpleScannerCallback {
+                override fun onScanResult(callbackType: Int, scanResult: ScanResult) {
+                    val device = scanResult.getDevice()
+                    val rssi = scanResult.rssi
+                    Log.d("MainActivity", "Found Device: " + device.toString() + " Rssi: " + rssi.toString())
+                    //Originally I asked to show all the found rssi's
+                    //editText.append("Device: " + device.toString() + " rssi: " + rssi.toString() + "\n")
+                }
+
+                override fun onBatchScanResults(scanResults: List<ScanResult>) {
+                    Log.d("MainActivity", "onBatchScanResults(): " + Arrays.toString(scanResults.toTypedArray()))
+                }
+
+                override fun onFinish() {
+                    Log.d("MainActivity", "onFinish()")
+                }
+
+                override fun onScanFailed(errorCode: Int) {
+                    Log.d("MainActivity", "onScanFailed() $errorCode")
+                }
+            })
+        //}
     }
+
     private fun randomGenerate(): Int {
 
         val bound = 65536
         //generate random values from 0-65536
         return Random(System.nanoTime()).nextInt(bound)
     }
+
+
+    //lets check what happened with the permissions requested...
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        when (requestCode) {
+            PERMISSION_REQUEST_COARSE_LOCATION -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    println("coarse location permission granted")
+                } else {
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                    builder.setTitle("Functionality limited")
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.")
+                    builder.setPositiveButton("OK", null)
+                    builder.show()
+                }
+                return
+            }
+            PERMISSION_REQUEST_FINE_LOCATION -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    println("fine location permission granted")
+                } else {
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                    builder.setTitle("Functionality limited")
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.")
+                    builder.setPositiveButton("OK", null)
+                    builder.show()
+                }
+                return
+            }
+        }
+    }
+
+    
 }
